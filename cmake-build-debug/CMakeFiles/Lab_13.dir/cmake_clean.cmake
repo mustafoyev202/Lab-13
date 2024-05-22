c38834da79e9328acc98a09de55c9afb1e7a99fa
MuxtorovicH <rusdosmux@gmail.com>
2024-05-22 23:45:11 +0500

    Lab13

---
+++ cmake-build-debug/CMakeFiles/Lab_13.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_13.dir/main.cpp.obj"
+  "CMakeFiles/Lab_13.dir/main.cpp.obj.d"
+  "Lab_13.exe"
+  "Lab_13.exe.manifest"
+  "Lab_13.pdb"
+  "libLab_13.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
